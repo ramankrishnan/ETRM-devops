f023a80e332811aaee94f522c56e5de8a570b907
Raman <107201500+ramankrishnan@users.noreply.github.com>
2025-09-21 12:07:48 +0530

    Update test_dummy.py
    updated3

--- trade-capture/app/tests/test_dummy.py
+++ trade-capture/app/tests/test_dummy.py
@@ -1,6 +1,7 @@
+# trade-capture/app/tests/test_dummy.py
+
+from app.main import app   # <-- absolute import
 from fastapi.testclient import TestClient
-from app.main import app
-from ..main import app
 
 client = TestClient(app)
 
